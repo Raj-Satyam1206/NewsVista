513fce36db86d664a4057bc836dc7dcc8e6d4bbe
Satyam Raj <raj.satyam1229@gmail.com>
2025-01-29 03:37:44 +0530

    Remove commented and unused code for cleanup

--- src/App.js
+++ src/App.js
@@ -2,30 +2,15 @@ import './App.css';
 import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import News from './components/News';
-// import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-// import Alert from './components/Alert';
 
 const App = ()=> {
   const pageSize = 5;
   const apiKey = process.env.REACT_APP_NEWS_API;
+  const country = "us";
   const [progress, setProgress] = useState(0);
-
-
   const [mode,setMode] = useState('light');
-  // const [alert , setAlert] = useState(null);
-
-  // const showAlert = (message,type) => {
-  //   setAlert({
-  //     msg: message,
-  //     type:type
-  //   })
-
-  //   setTimeout(() => {
-  //       setAlert(null);
-  //   }, 1500);
-  // }
 
   const toggleMode = () => {
     if(mode === "light"){
@@ -49,7 +34,7 @@ const App = ()=> {
             <>
               <NavBar mode={mode} toggleMode={toggleMode} />
               <LoadingBar height={3} color="#f11946" progress={progress} />
-              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />
+              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country={country} category="general" />
             </>
           }
         />
@@ -58,7 +43,7 @@ const App = ()=> {
           element={
             <>
               <NavBar mode={mode} toggleMode={toggleMode} />
-              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />
+              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country={country} category="business" />
             </>
           }
         />
@@ -67,7 +52,7 @@ const App = ()=> {
           element={
             <>
               <NavBar mode={mode} toggleMode={toggleMode} />
-              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />
+              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country={country} category="entertainment" />
             </>
           }
         />
@@ -76,7 +61,7 @@ const App = ()=> {
           element={
             <>
               <NavBar mode={mode} toggleMode={toggleMode} />
-              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />
+              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country={country} category="health" />
             </>
           }
         />
@@ -85,7 +70,7 @@ const App = ()=> {
           element={
             <>
               <NavBar mode={mode} toggleMode={toggleMode} />
-              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />
+              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country={country} category="science" />
             </>
           }
         />
@@ -94,7 +79,7 @@ const App = ()=> {
           element={
             <>
               <NavBar mode={mode} toggleMode={toggleMode} />
-              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />
+              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country={country} category="sports" />
             </>
           }
         />
@@ -103,7 +88,7 @@ const App = ()=> {
           element={
             <>
               <NavBar mode={mode} toggleMode={toggleMode} />
-              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />
+              <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country={country} category="technology" />
             </>
           }
         />
@@ -126,122 +111,3 @@ const App = ()=> {
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //createBrwoeserRouter
-  // const router = createBrowserRouter([
-  //   {
-  //     path: '/',
-  //     element: <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/> ,
-  //   },
-  //   {
-  //     path: "/business",
-  //     element: <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />,
-  //   },
-  //   {
-  //     path: "/entertainment",
-  //     element: <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />,
-  //   },
-  //   {
-  //     path: "/health",
-  //     element: <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />,
-  //   },
-  //   {
-  //     path: "/science",
-  //     element: <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />,
-  //   },
-  //   {
-  //     path: "/sports",
-  //     element: <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />,
-  //   },
-  //   {
-  //     path: "/technology",
-  //     element: <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />,
-  //   },
-  // ]);
-  // const router = createBrowserRouter(
-  //   createRoutesFromElements(
-  //     <>
-  //       <Route >
-  //         <Route
-  //           path = "/"
-  //           element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />}
-  //         />
-  //         <Route
-  //           path="business"
-  //           element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />}
-  //         />
-  //         <Route
-  //           path="entertainment"
-  //           element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />}
-  //         />
-  //         <Route
-  //           path="health"
-  //           element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />}
-  //         />
-  //         <Route
-  //           path="science"
-  //           element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />}
-  //         />
-  //         <Route
-  //           path="sports"
-  //           element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />}
-  //         />
-  //         <Route
-  //           path="technology"
-  //           element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />}
-  //         />
-  //       </Route>
-  //     </>
-  //   )
-  // );
-
-
-
-
-
-
-
-
-
-
-
-
-//SWITCH  
-//     return (
-//       <div>
-//         {/* <Router> */}
-//           <NavBar mode={mode} toggleMode={toggleMode} /> 
-//           <LoadingBar
-//             height={3}
-//             color='#f11946'
-//             progress={progress} 
-//           />
-// {/*       
-//         <Switch>
-//           <Route exact path="/">
-//             <News mode={mode} setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>
-//           </Route> 
-//           <Route exact path="/business"><News mode={mode} setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/></Route> 
-//           <Route exact path="/entertainment"><News mode={mode} setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/></Route> 
-//           <Route exact path="/general"><News mode={mode} setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
-//           <Route exact path="/health"><News mode={mode} setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"/></Route> 
-//           <Route exact path="/science"><News mode={mode} setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/></Route> 
-//           <Route exact path="/sports"><News mode={mode} setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/></Route> 
-//           <Route exact path="/technology"><News mode={mode} setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/></Route> 
-//         </Switch> */}
-//         {/* </Router> */}
-//       </div>
-//     )
